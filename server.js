038bb2810608786e39607f53b3572e950d11cdd2
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-07-04 01:21:10 +0100

    recommit

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/Books');
+
+// express app
+const app = express();
+
+const PORT = 4000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use('/api/books', bookRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(PORT, () => {
+      console.log(`Connected to db & listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
